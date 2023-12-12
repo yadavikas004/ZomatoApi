901667f0f3a5759978451058e4caa68dc0254ee1
Vikas Yadav <yadavikas088@gmail.com>
2023-12-12 10:13:31 +0530

    (development): api created using apache kafka

---
+++ DeliveryBody/src/main/java/com/deliveryboy/deliverybody/DeliveryBodyApplication.java
@@ -0,0 +1,14 @@
+package com.deliveryboy.deliverybody;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryBodyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryBodyApplication.class, args);
+		System.out.println("--------------Application Started--------------");
+	}
+
+}
---
+++ DeliveryBody/src/test/java/com/deliveryboy/deliverybody/DeliveryBodyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deliveryboy.deliverybody;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryBodyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
